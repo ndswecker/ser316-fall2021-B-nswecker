87e49630204fc971b6a3bb8c0e8616074575c4e4
Nathanael Swecker <you@example.com>
2021-11-01 19:09:02 -0700

    Create calculateSavings class
    create a new test class that only tests calculateSavings() method in
    BearWorkshop.java. Modeled my test case from work done in SER 216

---
+++ src/test/java/CalculateSavingsTest.java
@@ -0,0 +1,30 @@
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import main.*;
+
+/**
+Class:	CalculateSavingsTest
+
+Description: Method to test calculateSavings() from BearWorkshop
+*/
+public class CalculateSavingsTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	public void testThisClass() {
+		System.out.println("CalculateSavings() is running. ");
+	}
+
+}
