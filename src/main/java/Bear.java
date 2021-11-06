6fb131645a0135a50c006532333ffaa88481af4c
Nathanael Swecker <you@example.com>
2021-11-06 15:04:49 -0700

    Checkstyle for Bear.java
    Chaninging checkstyle errors like tabs, javadoc

--- src/main/java/Bear.java
+++ src/main/java/Bear.java
@@ -1,7 +1,7 @@
 /*
-File:	Bear.java
-Author:	SER 316
-Date:	Fall B 2021
+File:   Bear.java
+Author: SER 316
+Date:   Fall B 2021
   
 Description:
 */
@@ -12,8 +12,7 @@ import java.util.LinkedList;
 import main.java.Stuffing.stuffing;
 
 /**
- * Class: Bear
- * 
+ * Class: Bear.
  * Description:
  */
 public class Bear implements Comparable<Bear> {
@@ -27,7 +26,9 @@ public class Bear implements Comparable<Bear> {
     // bear has stuffing (req)
     // bear has a tattoo/emroider or not (opt)
     // bear has a noisemaker (opt)
-
+/**
+ * Default constructor for Bear class.
+ */
     public Bear() {
         this.casing = new Casing();
         this.stuff = new Stuffing(stuffing.BASE);
@@ -36,7 +37,10 @@ public class Bear implements Comparable<Bear> {
         ink = new Embroidery("");
         price = 0;
     }
-
+/**
+ * Start with stuffing constructor for Bear class
+ * @param stuff is of class Stuffing 
+*/
     public Bear(stuffing stuff) {
         this.casing = new Casing();
         this.stuff = new Stuffing(stuff);
@@ -47,20 +51,17 @@ public class Bear implements Comparable<Bear> {
     }
 
     /**
-     * Method: setPrice() Description: To be filled in
+     * Method: setPrice() Description: To be filled in.
      * 
      * @param incomingPrice
-     * @return void
      */
     public void setPrice(double incomingPrice) {
         this.price = incomingPrice;
     }
 
     /**
-     * Method: addNoise() Description: Adds NoiseMaker object to Bear object
-     * 
-     * @param noise
-     * @return boolean
+     * Method: addNoise() Description: Adds NoiseMaker object to Bear object.
+     * @param noise is of class Noise
      */
     public boolean addNoise(NoiseMaker noise) {
         if (this.noisemakers.size() >= 5) {
@@ -77,11 +78,8 @@ public class Bear implements Comparable<Bear> {
     }
 
     /**
-     * Method: compareTo() Description: Compares incoming bear price to this bear
-     * price
-     * 
-     * @param bear
-     * @return int
+     * Method: compareTo() Description: Compares incoming bear price to this bear price.
+     * @param bear of class Bear
      */
     @Override
     public int compareTo(Bear bear) {
