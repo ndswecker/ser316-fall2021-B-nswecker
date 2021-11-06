844586dd59f0ff3bab119c742f541918a3fe3c5d
Nathanael Swecker <you@example.com>
2021-11-06 15:33:14 -0700

    Embroidery.java checkstyle error fixes

--- src/main/java/Embroidery.java
+++ src/main/java/Embroidery.java
@@ -1,7 +1,7 @@
 /*
-  File:	Embroidery.java
-  Author:	SER 316
-  Date:	Fall B 2021
+  File:     Embroidery.java
+  Author:   SER 316
+  Date:     Fall B 2021
   
   Description:
 */
@@ -12,14 +12,16 @@ package main.java;
 
 /**
  * Class: Embroidery
- * 
- * Description:
+ * Description: A string may be "embroidered" on a bear using ink.
  */
 public class Embroidery {
     final double pricePerLetter = 1.00;
     double price;
     String embroidText;
-
+    /** 
+     * Only constructor allowed
+     * @param embroidery is a String that will be added to the bear. 
+     */
     public Embroidery(String embroidery) {
         this.embroidText = embroidery;
         this.price = embroidery.length() * pricePerLetter;
